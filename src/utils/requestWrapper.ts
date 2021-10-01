fdf2ba13220641f8c2a46abfdbd67e840a004eb2
Henrique <henrique@falconer.com.br>
2021-10-01 11:22:38 -0300

    feat: add requestWrapper retries

--- src/utils/requestWrapper.ts
+++ src/utils/requestWrapper.ts
@@ -6,7 +6,8 @@ import RateLimitError from 'errors/RateLimitError';
 import { wait } from './time';
 
 const requestWrapper = async <D>(
-  request: () => Promise<OctokitResponse<D>>
+  request: () => Promise<OctokitResponse<D>>,
+  retries = 1
 ): Promise<D> => {
   try {
     const { data, headers } = await request();
@@ -16,7 +17,11 @@ const requestWrapper = async <D>(
 
     return data;
   } catch (err: any) {
-    if (err.response.headers['x-ratelimit-remaining'] !== '0') throw err;
+    if (err.response?.headers['x-ratelimit-remaining'] !== '0') {
+      console.log(err);
+      if (retries) return requestWrapper(request, --retries);
+      throw err;
+    }
 
     const resetMoment = moment.unix(
       Number(err.response.headers['x-ratelimit-reset']) + 1
@@ -30,7 +35,7 @@ const requestWrapper = async <D>(
 
     await wait(resetMoment.diff(moment()));
 
-    return requestWrapper(request);
+    return requestWrapper(request, retries);
   }
 };
 
