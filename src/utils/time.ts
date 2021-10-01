dc27ff4691172f71e6abf6ec84bcf819abf7da0a
Henrique <henrique@falconer.com.br>
2021-10-01 03:42:06 -0300

    feat: create getPreviousWeek

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,2 +1,7 @@
+import moment from 'moment';
+
 export const wait = (milliseconds: number) =>
   new Promise((res) => setTimeout(res, milliseconds));
+
+export const getPreviousWeek = (date: string) =>
+  moment(date, 'YYYY-MM-DD').subtract(1, 'week').format('YYYY-MM-DD');
