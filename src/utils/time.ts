9c7b8b47e1c2dd8ad858717b2f3db9d7164ef948
Henrique <henrique@falconer.com.br>
2021-10-03 01:00:43 -0300

    feat: create waitUntil & raceWaitUntil

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,6 +3,19 @@ import moment from 'moment';
 export const wait = (milliseconds: number) =>
   new Promise<void>((res) => setTimeout(res, milliseconds));
 
+export const waitUntil = (time: moment.Moment) =>
+  new Promise<void>((res) => {
+    const ref = setInterval(() => {
+      if (moment().isAfter(time)) {
+        clearInterval(ref);
+        res();
+      }
+    }, 1000);
+  });
+
+export const raceWaitUntil = (time: moment.Moment) =>
+  Promise.race([wait(time.diff(moment())), waitUntil(time)]);
+
 export const getPreviousWeek = (date: string) =>
   moment(date, 'YYYY-MM-DD').subtract(1, 'week').format('YYYY-MM-DD');
 
